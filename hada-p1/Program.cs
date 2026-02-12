7354d9b2a140d9ccb97e852519c1a062412ce779
Manuel Sánchez <msesclapez1@gmail.com>
2026-02-12 16:57:09 +0100

    Añadido interaccion con el usuario

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace hada_p1
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string opcion;
+
+            do
+            {
+                Console.WriteLine("¿Convertir desde (s) segundos o (m) minutos?");
+                string unidad = Console.ReadLine();
+
+                Console.WriteLine("Introduce cantidad:");
+                double valor = double.Parse(Console.ReadLine());
+
+                if (unidad == "s")
+                    Console.WriteLine(HadaP1.Seconds2Minutes(valor) + " minutos");
+                else
+                    Console.WriteLine(HadaP1.Minutes2Seconds(valor) + " segundos");
+
+                Console.WriteLine("¿Otra conversión? (y/n)");
+                opcion = Console.ReadLine();
+
+            } while (opcion == "y");
         }
     }
 }
